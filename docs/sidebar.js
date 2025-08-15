8dbe7696808c9195efee455fc36dadc9b7882d71
hsleiendecker <h.s.leiendecker@gmail.com>
2025-08-15 13:35:41 -0700

    site JS sidebar test

---
+++ docs/sidebar.js
@@ -0,0 +1,4 @@
+function loadNavbarDiv() {
+     String navbar_code_str = '<nav><div class="w3-sidebar w3-bar-block" style="width:18%"><h3 class="w3-bar-item">Contents</h3> <p><a href="/fargo3d-DustRad/dust_evo.html" class="w3-bar-item w3-button"> Dust Evolution</a> </p><p><a href="/fargo3d-DustRad/readme.html" class="w3-bar-item w3-button">Original FARGO3D Doc</a></p><p><a href="/fargo3d-DustRad/contact.html class=" class="w3-bar-item w3-button">Contact</a></p></div></nav>'
+     $('body').append(navbar_code_str);
+}
